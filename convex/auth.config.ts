0d510ce3a09eb999f9479f3210c7a88a9d63262e
Ayush Bansal <mr.bansal@Ayushs-MacBook-Air-3.local>
2024-09-25 13:17:31 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://closing-whippet-68.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
